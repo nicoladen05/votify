16fa380f8a191d4cab4ff914ed32bdcfc85a4a79
tobihxd <towi06@icloud.com>
2026-02-16 15:31:03 +0100

    wip: spotify auth

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { PUBLIC_SPOTIFY_CLIENT_ID } from '$env/static/public';
+	const queryparams = {
+		response_type: 'code',
+		redirect_uri: 'http://127.0.0.1:5173/admin/spotify/callback',
+		scope: 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
+	};
+	const url =
+		`https://accounts.spotify.com/authorize?client_id=${PUBLIC_SPOTIFY_CLIENT_ID}` +
+		`&response_type=${queryparams.response_type}` +
+		`&redirect_uri=${queryparams.redirect_uri}` +
+		`&scope=${encodeURIComponent(queryparams.scope)}`;
+</script>
+
+<button
+	onclick={() => {
+		window.location.href = url;
+	}}
+	>LogIn with Spotify
+</button>
---
+++ src/routes/admin/spotify/callback/+server.ts
@@ -0,0 +1,28 @@
+import { SPOTIFY_CLIENT_SECRET } from '$env/static/private';
+import { PUBLIC_SPOTIFY_CLIENT_ID } from '$env/static/public';
+import { json, redirect } from '@sveltejs/kit';
+
+export async function GET({ url }) {
+	const code = url.searchParams.get('code');
+	if (!code) throw redirect(303, '/admin');
+
+	const header = {
+		Authorization: btoa(`${PUBLIC_SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`),
+		'Content-Type': 'application/x-www-form-urlencoded'
+	};
+
+	const body = new URLSearchParams({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: 'http://127.0.0.1:5173/admin/spotify/callback'
+	});
+
+	const response = await fetch('https://accounts.spotify.com/api/token', {
+		headers: header,
+		method: 'POST',
+		body: body
+	});
+	if (!response.ok)
+		return json({ success: false, message: 'Failed to get authorization Token' }, { status: 500 });
+	const data = response.json();
+}
