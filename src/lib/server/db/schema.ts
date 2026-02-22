603df3925b88e19a5fbe9b7b45acb5eb7d1d79d8
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-02-22 11:00:47 +0100

    feat: add room_id to songQueueItem and vote

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -6,8 +6,7 @@ import {
 	boolean,
 	foreignKey,
 	serial,
-	integer,
-	pgEnum
+	integer
 } from 'drizzle-orm/pg-core';
 import { user } from './auth.schema';
 
@@ -28,13 +27,18 @@ export const spotifyTokens = pgTable(
 	]
 );
 
-export const songQueueItem = pgTable('songQueueItem', {
-	song_id: text('song_id').notNull().primaryKey(),
-	song_uri: text('song_uri').notNull(),
-	img_url: text('img_url').notNull(),
-	title: text('title').notNull(),
-	artist: text('artist').notNull()
-});
+export const songQueueItem = pgTable(
+	'songQueueItem',
+	{
+		room_id: integer('room_id').notNull(),
+		song_id: text('song_id').notNull(),
+		song_uri: text('song_uri').notNull(),
+		img_url: text('img_url').notNull(),
+		title: text('title').notNull(),
+		artist: text('artist').notNull()
+	},
+	(table) => [primaryKey({ columns: [table.room_id, table.song_id] })]
+);
 
 export const guest = pgTable('guest', {
 	cookie: text('cookie').primaryKey() // Cookie ist eindeutige ID
@@ -43,16 +47,17 @@ export const guest = pgTable('guest', {
 export const votes = pgTable(
 	'votes',
 	{
+		room_id: integer('room_id').notNull(),
 		song_id: text('song_id').notNull(),
 		guest_cookie: text('guest_cookie').notNull(),
 		is_upvote: boolean('is_upvote').notNull()
 	},
 	(table) => [
-		primaryKey({ columns: [table.song_id, table.guest_cookie] }),
+		primaryKey({ columns: [table.room_id, table.song_id, table.guest_cookie] }),
 
 		foreignKey({
-			columns: [table.song_id],
-			foreignColumns: [songQueueItem.song_id]
+			columns: [table.room_id, table.song_id],
+			foreignColumns: [songQueueItem.room_id, songQueueItem.song_id]
 		}).onDelete('cascade'),
 
 		foreignKey({
