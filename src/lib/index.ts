d222a38c55985769135dcc364595ae34e01d180e
Nicolas Hartmann <nicolas.hartmann@ptb.de>
2026-02-23 09:43:27 +0100

    fix: client_id spelling

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,7 @@
 import { PUBLIC_SPOTIFY_CLIENT_ID } from '$env/static/public';
 
 const queryparams = new URLSearchParams({
-	cliend_id: PUBLIC_SPOTIFY_CLIENT_ID,
+	client_id: PUBLIC_SPOTIFY_CLIENT_ID,
 	response_type: 'code',
 	redirect_uri: 'http://127.0.0.1:5173/admin/spotify/callback',
 	scope: 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
