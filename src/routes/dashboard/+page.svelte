bae987ad9a584b7846d43f28632b877573e7fd0c
tobihxd <towi06@icloud.com>
2026-02-25 14:11:09 +0100

    Refactor: Improve room and connection components

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,23 +1,10 @@
 <script lang="ts">
-	import { resolve } from '$app/paths';
-	import {
-		ChartColumnIncreasing,
-		CrownIcon,
-		LockIcon,
-		PlayIcon,
-		PlusIcon,
-		RadioIcon,
-		Settings2Icon,
-		SparklesIcon,
-		Trash2Icon,
-		ZapIcon
-	} from '@lucide/svelte';
+	import { CrownIcon, LockIcon, PlusIcon, SparklesIcon, ZapIcon } from '@lucide/svelte';
 	import Button from '$lib/compenents/ui/Button.svelte';
 	import CreateRoomDialog from '$lib/compenents/dashboard/CreateRoomDialog.svelte';
 	import type { PageProps } from './$types';
-	import { enhance } from '$app/forms';
-	import { spotifyAuthUrl } from '$lib';
 	import { getMobileSidebarOpenButton } from '$lib/context/sidebar-context';
+	import RoomCard from '$lib/compenents/dashboard/RoomCard.svelte';
 
 	type Plan = 'free' | 'pro' | 'premium';
 
@@ -81,58 +68,7 @@
 
 <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
 	{#each rooms as room (room)}
-		<div class="card-hover rounded-xl border border-border bg-secondary p-6">
-			<div class="mb-4 flex items-center justify-between">
-				<h3 class="font-bold text-foreground">{room.name}</h3>
-				<!-- <span
-					class={`inline-flex items-center gap-1 rounded-full px-2.5 py-0.5 text-xs font-medium ${
-						room.status === 'live'
-							? 'bg-accent/20 text-accent'
-							: 'bg-secondary text-muted-foreground'
-					}`}
-				>
-					<span
-						class={`h-1.5 w-1.5 rounded-full ${
-							room.status === 'live' ? 'animate-pulse bg-accent' : 'bg-muted-foreground'
-						}`}
-					></span>
-					{room.status === 'live' ? 'Live' : 'Offline'}
-				</span> -->
-			</div>
-			<div class="flex items-center gap-2">
-				<a href={resolve(`/room/${room.id}`)}>
-					<Button variant="hero" size="sm">
-						<PlayIcon class="mr-1 h-3 w-3" />
-						Launch
-					</Button>
-				</a>
-				<Button
-					variant="ghost"
-					size="icon"
-					class="h-8 w-8 text-muted-foreground hover:bg-transparent hover:text-foreground"
-				>
-					<Settings2Icon class="h-4 w-4" />
-				</Button>
-				<Button
-					variant="ghost"
-					size="icon"
-					class="h-8 w-8 text-muted-foreground hover:bg-transparent hover:text-foreground"
-				>
-					<ChartColumnIncreasing class="h-4 w-4" />
-				</Button>
-				<form method="post" action="?/deleteRoom" use:enhance>
-					<input value={room.id} name="room-id" type="hidden" />
-					<Button
-						variant="ghost"
-						type="submit"
-						size="icon"
-						class="hover:color-destructive h-8 w-8 text-muted-foreground hover:bg-transparent"
-					>
-						<Trash2Icon class="h-4 w-4" />
-					</Button>
-				</form>
-			</div>
-		</div>
+		<RoomCard {room} action="?/deleteRoom" />
 	{/each}
 
 	{#if rooms.length >= maxRooms}
@@ -150,53 +86,6 @@
 		</div>
 	{/if}
 </div>
-
-<!-- {#if !data.hasConnectedSpotify} -->
-<div
-	class="mt-10 flex flex-col items-start justify-between gap-4 rounded-xl border border-border bg-secondary p-6 sm:flex-row sm:items-center"
->
-	<div class="flex items-center gap-4">
-		{#if !data.hasConnectedSpotify}
-			<div class="flex h-12 w-12 items-center justify-center rounded-full bg-accent/10">
-				<RadioIcon class="h-6 w-6 text-accent" />
-			</div>
-			<div>
-				<h3 class="font-bold text-foreground">Spotify Connection</h3>
-				<p class="text-sm text-muted-foreground">
-					Connect your account to start playing music in rooms.
-				</p>
-			</div>
-		{:else}
-			<div class="flex h-12 w-12 items-center justify-center rounded-full bg-accent/10">
-				<img
-					src={data.user?.image_url}
-					alt="User avatar"
-					class="h-12 w-12 rounded-full object-cover"
-				/>
-			</div>
-			<div>
-				<p class="font-bold text-foreground">{data.user?.name}</p>
-				<p class="text-sm text-muted-foreground">Authorized Spotify account</p>
-			</div>
-		{/if}
-	</div>
-
-	{#if !data.hasConnectedSpotify}
-		<Button
-			variant="hero"
-			size="sm"
-			onclick={() => {
-				window.location.href = spotifyAuthUrl;
-			}}>Connect Spotify</Button
-		>
-	{:else}
-		<form method="post" action="?/logoutSpotify" class="flex justify-end" use:enhance>
-			<Button type="submit" variant="destructive" size="sm">Disconnect Spotify</Button>
-		</form>
-	{/if}
-</div>
-<!-- {/if} -->
-
 <CreateRoomDialog
 	open={isCreateRoomDialogOpen}
 	onClose={closeCreateRoomDialog}
--- src/routes/room/[roomId]/+page.server.ts
+++ src/routes/room/[roomId]/+page.server.ts
@@ -1,16 +1,24 @@
 import { db } from '$lib/server/db';
-import { room as roomTable } from '$lib/server/db/schema';
+import { room as roomTable, spotifyTokens } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
+import { getAccessTokenByTokenId } from '$lib/server/spotify';
 
 export const load: PageServerLoad = async ({ params, locals }) => {
 	const roomIdParam = parseInt(params.roomId);
 
 	if (!Number.isInteger(roomIdParam) || roomIdParam <= 0) error(404, 'This room does not exist');
 
-	const rooms = await db.select().from(roomTable).where(eq(roomTable.id, roomIdParam));
-	const room = rooms[0];
+	const dbrooms = await db.select().from(roomTable).where(eq(roomTable.id, roomIdParam));
+	const room = dbrooms[0];
+
+	const spotifyAccount = room.spotifyTokens ? await getAccountData(room.spotifyTokens!) : null;
+
+	const getStatus = () => {
+		if (!spotifyAccount) return 'missing_credentials';
+		return 'live';
+	};
 
 	if (!room) {
 		return error(404, 'This room does not exist');
@@ -22,8 +30,43 @@ export const load: PageServerLoad = async ({ params, locals }) => {
 
 	return {
 		room: {
+			roomid: roomIdParam,
 			name: room.name,
-			status: 'live'
-		}
+			status: getStatus()
+		},
+		spotifyAccount
 	};
+};
+
+async function getAccountData(
+	accountid: number
+): Promise<{ name: string; image_url: string } | null> {
+	const spotifyCredentials = await db
+		.select()
+		.from(spotifyTokens)
+		.where(eq(spotifyTokens.id, accountid));
+
+	if (spotifyCredentials.length === 1) {
+		console.log(spotifyCredentials);
+		console.log(await getAccessTokenByTokenId(spotifyCredentials[0].id));
+		const userProfileRequest = await fetch('https://api.spotify.com/v1/me', {
+			headers: {
+				Authorization: `Bearer ${await getAccessTokenByTokenId(spotifyCredentials[0].id)}`
+			}
+		});
+		console.log(userProfileRequest);
+		if (!userProfileRequest.ok) return null;
+
+		const userProfile = await userProfileRequest.json();
+
+		return { name: userProfile.display_name, image_url: userProfile.images[0].url };
+	} else {
+		return null;
+	}
+}
+
+export const actions: Actions = {
+	logoutSpotify: async () => {
+		await db.delete(spotifyTokens);
+	}
 };
