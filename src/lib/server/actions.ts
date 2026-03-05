65e60ad8ba93e780fe39891a03d22fab41a2e295
Nicolas Hartmann <nicolas.hartmann@ptb.de>
2026-03-05 09:36:07 +0100

    fix: add user validation to room form action

--- src/lib/server/actions.ts
+++ src/lib/server/actions.ts
@@ -1,4 +1,4 @@
-import type { RequestEvent } from '@sveltejs/kit';
+import { fail, type RequestEvent } from '@sveltejs/kit';
 import { and, eq } from 'drizzle-orm';
 import { db } from './db';
 import { room } from './db/schema';
@@ -7,19 +7,36 @@ export async function launchRoom(event: RequestEvent) {
 	const formData = await event.request.formData();
 	const id = parseInt(formData.get('room-id') as string);
 	const spotifyid = parseInt(formData.get('spotify-id') as string);
+	const userId = event.locals.user?.id;
+
 	if (!spotifyid) return;
-	await db
-		.update(room)
-		.set({ state: 'offline' })
-		.where(and(eq(room.spotifyTokens, spotifyid), eq(room.state, 'live')));
-	await db.update(room).set({ state: 'live' }).where(eq(room.id, id));
+	if (!userId) return fail(403);
+
+	await Promise.all([
+		(db
+			.update(room)
+			.set({ state: 'offline' })
+			.where(
+				and(eq(room.spotifyTokens, spotifyid), eq(room.state, 'live'), eq(room.userId, userId))
+			),
+		db
+			.update(room)
+			.set({ state: 'live' })
+			.where(and(eq(room.id, id), eq(room.userId, userId))))
+	]);
 }
 
 export async function stopRoom(event: RequestEvent) {
 	const formData = await event.request.formData();
 	const id = parseInt(formData.get('room-id') as string);
 
-	await db.update(room).set({ state: 'offline' }).where(eq(room.id, id));
+	const userId = event.locals.user?.id;
+	if (!userId) return fail(403);
+
+	await db
+		.update(room)
+		.set({ state: 'offline' })
+		.where(and(eq(room.id, id), eq(room.userId, userId)));
 }
 
 export async function selectAccount(event: RequestEvent) {
@@ -27,8 +44,11 @@ export async function selectAccount(event: RequestEvent) {
 	const roomid = parseInt(formData.get('room-id') as string);
 	const accountid = parseInt(formData.get('account-id') as string);
 
+	const userId = event.locals.user?.id;
+	if (!userId) return fail(403);
+
 	await db
 		.update(room)
 		.set({ spotifyTokens: accountid, state: 'offline' })
-		.where(eq(room.id, roomid));
+		.where(and(eq(room.id, roomid), eq(room.userId, userId)));
 }
